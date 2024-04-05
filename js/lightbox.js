a78ed4a059145ee806d73b72f5b4cadb9967c58d
Iagooalmeida <iago732@gmail.com>
2024-04-05 03:52:03 -0300

    finalizado galeria com modal

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona todas as imagens com a classe lightbox-trigger
     const lightboxImages = document.querySelectorAll('.lightbox-trigger');
-
-    // Seleciona o modal do lightbox e o botão de fechar
     const lightboxModal = document.getElementById('lightbox-modal');
     const closeButton = document.querySelector('.close-btn');
 
     // Adiciona um evento de clique a cada imagem
     lightboxImages.forEach(function (image) {
         image.addEventListener('click', function () {
-            // Atualiza o conteúdo da imagem no modal do lightbox
             const modalImage = document.getElementById('lightbox-image');
             modalImage.src = this.src;
-            
-            // Exibe o modal do lightbox
             lightboxModal.style.display = 'block';
         });
     });
 
-    // Adiciona um evento de clique ao botão de fechar
+    // Fecha o modal quando o usuário clica no botão de fechar
     closeButton.addEventListener('click', function () {
-        // Fecha o modal do lightbox
         lightboxModal.style.display = 'none';
+    });
+
+    // Fecha o modal quando o usuário clica em qualquer área fora da imagem
+    lightboxModal.addEventListener('click', function (event) {
+        if (event.target === lightboxModal) {
+            lightboxModal.style.display = 'none';
+        }
     });
 });
