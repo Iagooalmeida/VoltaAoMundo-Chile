e20a6a8da12c2f467260cf65b0b8c6f2dcd1826e
Iagooalmeida <iago732@gmail.com>
2024-04-05 15:00:48 -0300

    adicionado funcionalidade de navegação de imagens

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const lightboxImages = document.querySelectorAll('.lightbox-trigger');
     const lightboxModal = document.getElementById('lightbox-modal');
     const closeButton = document.querySelector('.close-btn');
+    const modalImage = document.getElementById('lightbox-image');
+
+    let currentImageIndex = 0;
 
     // Adiciona um evento de clique a cada imagem
-    lightboxImages.forEach(function (image) {
+    lightboxImages.forEach(function (image, index) {
         image.addEventListener('click', function () {
-            const modalImage = document.getElementById('lightbox-image');
+            currentImageIndex = index;
             modalImage.src = this.src;
             lightboxModal.style.display = 'block';
         });
@@ -21,6 +24,19 @@ document.addEventListener('DOMContentLoaded', function () {
     lightboxModal.addEventListener('click', function (event) {
         if (event.target === lightboxModal) {
             lightboxModal.style.display = 'none';
+        }
+    });
+
+    // Adiciona event listener para as teclas de seta
+    document.addEventListener('keydown', function (event) {
+        if (lightboxModal.style.display === 'block') {
+            if (event.key === 'ArrowLeft') {
+                currentImageIndex = (currentImageIndex - 1 + lightboxImages.length) % lightboxImages.length;
+                modalImage.src = lightboxImages[currentImageIndex].src;
+            } else if (event.key === 'ArrowRight') {
+                currentImageIndex = (currentImageIndex + 1) % lightboxImages.length;
+                modalImage.src = lightboxImages[currentImageIndex].src;
+            }
         }
     });
 });
